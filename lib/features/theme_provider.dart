9f4d68baf4eb452fccdf0bcfef7d4faa903b7567
victorzarzar <victorzarzar58@gmail.com>
2024-09-30 07:28:59 -0300

    Refactor dark theme and main.dart

---
+++ lib/features/theme_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_list/features/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    _themeData = isDarkMode ? darkMode : lightMode;
+    notifyListeners();
+  }
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+    _saveTheme();
+  }
+
+  Future<void> _saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', isDarkMode);
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:grocery_list/features/dark_mode.dart';
+import 'package:grocery_list/features/theme_provider.dart';
 import 'package:grocery_list/screens/intro_page.dart';
 import 'package:provider/provider.dart';
 
@@ -18,7 +18,10 @@ void main() async {
       fallbackLocale: const Locale(
         'en-US',
       ),
-      child: const MyApp(),
+      child: ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        child: const MyApp(),
+      ),
     ),
   );
 }
@@ -33,25 +36,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => UiProvider()..init(),
-      child: Consumer<UiProvider>(
-        builder: (context, UiProvider notifier, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              useMaterial3: true,
-            ),
-            localizationsDelegates: context.localizationDelegates,
-            supportedLocales: context.supportedLocales,
-            locale: context.locale,
-            home: const IntroPage(),
-            themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
-            darkTheme:
-                notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
-          );
-        },
-      ),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: Provider.of<ThemeProvider>(context).themeData,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          home: const IntroPage(),
+        );
+      },
     );
   }
 }
