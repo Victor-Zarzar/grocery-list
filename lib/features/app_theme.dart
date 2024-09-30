168ff4035c95a5e77f61617d979c5707a186b57c
victorzarzar <victorzarzar58@gmail.com>
2024-09-30 07:32:40 -0300

    Feat: add app theme and assets app

---
+++ lib/features/app_theme.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static Color get primaryColor => const Color(0xFFCFD8DC);
+
+  static Color get secondaryColor => const Color(0x00000000);
+
+  static Color get thirdColor => const Color(0xFF212121);
+}
+
+class FontTextColor {
+  FontTextColor._();
+
+  static Color get primaryColor => const Color(0xFF263238);
+
+  static Color get secondaryColor => const Color(0xFFFFFFFF);
+
+  static Color get thirdColor => const Color(0xFF000000);
+}
+
+class ButtonColor {
+  ButtonColor._();
+
+  static Color get primaryColor => const Color(0xFF673AB7);
+
+  static Color get secondaryColor => const Color(0xFFCFD8DC);
+}
+
+class IconColor {
+  IconColor._();
+
+  static Color get primaryColor => const Color(0xFF000000);
+
+  static Color get secondaryColor => const Color(0xFF2196F3);
+
+  static Color get thirdColor => const Color(0xFFCFD8DC);
+}
+
+class SwitchColor {
+  SwitchColor._();
+
+  static Color get primaryColor => const Color(0xFFECEFF1);
+
+  static Color get secondaryColor => const Color(0xFFCFD8DC);
+}
