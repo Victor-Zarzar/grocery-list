0219d3b47de299d49266dcad3ec5240b650b1b5c
victorzarzar <victorzarzar58@gmail.com>
2024-10-01 20:25:14 -0300

    Update about page

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,10 +1,132 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_list/features/app_theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
 
   @override
+  State<AboutPage> createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw '${'launch_error'.tr()} $url';
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      backgroundColor: isDarkMode ? AppTheme.thirdColor : AppTheme.primaryColor,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: GFAppBar(
+                centerTitle: true,
+                backgroundColor:
+                    isDarkMode ? AppTheme.thirdColor : AppTheme.primaryColor,
+                title: Text(
+                  "about".tr(),
+                  style: GoogleFonts.jetBrainsMono(
+                    textStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: isDarkMode
+                          ? FontTextColor.secondaryColor
+                          : FontTextColor.primaryColor,
+                    ),
+                  ),
+                ),
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: isDarkMode
+                        ? FontTextColor.secondaryColor
+                        : FontTextColor.primaryColor,
+                    semanticLabel: 'backtopage'.tr(),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.info,
+                          color: IconColor.secondaryColor,
+                          semanticLabel: 'informationicon'.tr(),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Text(
+                            "abouttext".tr(),
+                            style: GoogleFonts.jetBrainsMono(
+                              textStyle: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: isDarkMode
+                                    ? FontTextColor.secondaryColor
+                                    : FontTextColor.primaryColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(40),
+              child: Center(
+                child: GestureDetector(
+                  onTap: () {
+                    _launchURL('https://www.victorzarzar.com.br');
+                  },
+                  child: Text(
+                    'developed'.tr(),
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.jetBrainsMono(
+                      textStyle: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: isDarkMode
+                            ? FontTextColor.secondaryColor
+                            : FontTextColor.primaryColor,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
